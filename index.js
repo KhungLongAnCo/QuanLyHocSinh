4436fcf7059ac5c0bfa971f5136a35d997b41899
Your Name <you@example.com>
2019-03-20 00:24:48 +0700

    remove login

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', 'views');
 app.use("/users", 
-	validateLogin.bin,
 	userRouter
 	);
 app.use('/auth', authRouter);
